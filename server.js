c1e3f1d1c413f6b72c5ae74e242b3a19d495b275
GnomeSaiyan <69124994+jeffreytwoods2@users.noreply.github.com>
2021-03-28 20:07:47 -0600

    Server.js now serves the correct file according to errors or results of mongodb submit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const Number = require("./numberModel");
 
@@ -9,19 +10,27 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/uniquenumdb", { useNewUrlParser: true });
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 app.post("/submit", ({body}, res) => {
     const submission = new Number(body);
 
     Number.create(submission)
-        .then(dbNumber => {
-            res.json(dbNumber);
+        .then(() => {
+            res.sendFile(path.join(__dirname, "/public/unique.html"));
         })
         .catch(err => {
-            res.json(err);
+            if (err.name === "ValidationError") {
+                res.sendFile(path.join(__dirname, "/public/notNum.html"));
+            } else if (err.name === "MongoError") {
+                res.sendFile(path.join(__dirname, "/public/notUnique.html"));
+            }
         });
 });
 
